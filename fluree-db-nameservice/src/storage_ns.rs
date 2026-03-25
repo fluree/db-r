5468b42ca187d99e9cdca45a181e6e4f4bfd092d
Benjamin Lamothe <bl@732.one>
2026-03-24 23:38:54 -0400

    fix bug resulting in silent success when updated branch didn't exist

--- fluree-db-nameservice/src/storage_ns.rs
+++ fluree-db-nameservice/src/storage_ns.rs
@@ -680,16 +680,26 @@ where
         let (ledger_name, branch) = split_ledger_id(ledger_id)?;
         let key = self.ns_key(&ledger_name, &branch);
 
-        self.cas_update::<NsFileV2, _>(&key, |existing| {
-            let mut file = existing?;
-            file.branch_point = Some(BranchPointRef {
-                source: new_branch_point.source.clone(),
-                commit_cid: Some(new_branch_point.commit_id.to_string()),
-                t: new_branch_point.t,
-            });
-            Some(file)
-        })
-        .await?;
+        let outcome = self
+            .storage
+            .compare_and_swap(&key, |bytes| {
+                let Some(data) = bytes else {
+                    return Ok(CasAction::Abort(()));
+                };
+                let mut file: NsFileV2 = deserialize_json(data)?;
+                file.branch_point = Some(BranchPointRef {
+                    source: new_branch_point.source.clone(),
+                    commit_cid: Some(new_branch_point.commit_id.to_string()),
+                    t: new_branch_point.t,
+                });
+                let new_bytes = serialize_json(&file)?;
+                Ok(CasAction::Write(new_bytes))
+            })
+            .await?;
+
+        if matches!(outcome, CasOutcome::Aborted(())) {
+            return Err(NameServiceError::not_found(ledger_id));
+        }
 
         Ok(())
     }
