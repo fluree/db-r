52adabd71f64d3550e1a62afc6e62c7e6fd37735
Benjamin Lamothe <bl@732.one>
2026-03-31 01:17:18 -0400

    remove unused import

--- fluree-db-api/src/rebase.rs
+++ fluree-db-api/src/rebase.rs
@@ -7,7 +7,7 @@
 use crate::error::{ApiError, Result};
 use fluree_db_core::ledger_id::format_ledger_id;
 use fluree_db_core::{
-    range_with_overlay, ConflictKey, ContentAddressedWrite, ContentId, Flake, IndexType,
+    range_with_overlay, ConflictKey, ContentId, Flake, IndexType,
     RangeMatch, RangeOptions, RangeTest, Storage,
 };
 use fluree_db_ledger::{LedgerState, LedgerView};
